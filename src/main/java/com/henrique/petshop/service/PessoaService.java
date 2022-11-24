144583013d594f869ba7e72869aedf8ceee89a6b
HenriqueJunio <henrique.junio@outlook.com>
2022-11-24 13:29:26 -0300

    POST, PUT e DELETE de Pessoa e Servico + finaAll

--- src/main/java/com/henrique/petshop/service/PessoaService.java
+++ src/main/java/com/henrique/petshop/service/PessoaService.java
@@ -1,12 +1,15 @@
 package com.henrique.petshop.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.henrique.petshop.domain.Pessoa;
 import com.henrique.petshop.repository.PessoaRepository;
+import com.henrique.petshop.service.exceptions.DataIntegrityException;
 import com.henrique.petshop.service.exceptions.ObjetoNaoEncontradoException;
 
 @Service
@@ -17,5 +20,29 @@ public class PessoaService {
 	public Pessoa find(Integer id) {
 		Optional<Pessoa> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjetoNaoEncontradoException("Objeto não encontrado. ID: "+id+", Tipo: "+Pessoa.class.getName()));
+	}
+	
+	public Pessoa insert(Pessoa obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Pessoa update(Pessoa obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Pessoa não pode ser deletada!");
+		}
+	}
+
+	public List<Pessoa> findAll() {
+		return repo.findAll();
 	}
 }
--- src/main/java/com/henrique/petshop/service/ServicoService.java
+++ src/main/java/com/henrique/petshop/service/ServicoService.java
@@ -3,10 +3,12 @@ package com.henrique.petshop.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.henrique.petshop.domain.Servico;
 import com.henrique.petshop.repository.ServicoRepository;
+import com.henrique.petshop.service.exceptions.DataIntegrityException;
 import com.henrique.petshop.service.exceptions.ObjetoNaoEncontradoException;
 
 @Service
@@ -17,5 +19,26 @@ public class ServicoService {
 	public Servico find(Integer id) {
 		Optional<Servico> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjetoNaoEncontradoException("Objeto não encontrado. ID: "+id+", Tipo: "+Servico.class.getName()));
+	}
+	
+	public Servico insert(Servico obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Servico update(Servico obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Servico possui produtos, não é possível deletar!");
+		}
+		
 	}
 }
